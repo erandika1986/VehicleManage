9435b96549517c5103e4a78e75e829b0a1bfb320
Ashen Dunusinghe <60811307+Avdunusinghe@users.noreply.github.com>
2021-06-21 10:31:50 +0530

    warehouse service implimented

--- vtApp/src/app/models/warehouse/warehouse.model.ts
+++ vtApp/src/app/models/warehouse/warehouse.model.ts
@@ -1,8 +1,18 @@
+import { User } from "../user/user.model";
+
 export class WarehouseModel
 {
     id:number;
     address:string;
-
-
+    phone:string;
+    ManagerId:string;
+    floorSpace:number;
+    createOn:Date
+    createById:number;
+    updateOn:Date;
+    updatedById:number;
     isActive:boolean;
+    SelectedManagerId:number;
+   
+    
 }
--- vtApp/src/app/services/warehouse/warehouse.service.ts
+++ vtApp/src/app/services/warehouse/warehouse.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ResponseModel } from 'app/models/common/response.model';
 import { WarehouseModel } from 'app/models/warehouse/warehouse.model';
 import { environment } from 'environments/environment';
 import { Observable } from 'rxjs';
@@ -14,5 +15,22 @@ export class WarehouseService {
   getWarehouseById(id: number): Observable<WarehouseModel> {
     return this.httpClient.
       get<WarehouseModel>(environment.apiUrl + 'Warehouse' + "/" + id);
+  }
+
+  GetAllWarehouses(): Observable<WarehouseModel[]> {
+    return this.httpClient.
+      get<WarehouseModel[]>(environment.apiUrl + 'Warehouse');
+  }
+
+  SaveWarehouse(vm: WarehouseModel): Observable<ResponseModel> {
+    return this.httpClient.
+      post<ResponseModel>(environment.apiUrl + 'Warehouse', vm);
+  }
+
+
+
+  delete(id: number): Observable<ResponseModel> {
+    return this.httpClient.
+      delete<ResponseModel>(environment.apiUrl + 'Warehouse' + "/" + id);
   }
 }
