850136e45dacb0de9ab18e9716a824b15616a7b0
Mahasen Ekanayake <mahasen.ekanayake@icloud.com>
2021-06-21 14:07:12 +0530

    CustomerService Updated

--- vtApp/src/app/models/customer/customer.model.ts
+++ vtApp/src/app/models/customer/customer.model.ts
@@ -1,5 +1,16 @@
 export class CustomerModel
 {
     id:number;
+    name:string;
+    description:string;
+    contactNo1:string;
+    contactNo2:string;
+    email:string;
+    address:string;
+    latitude:number;
+    longitude:number;
+    priority:number;
+    routeID:number;
+    isActive:boolean;
     
 }
--- vtApp/src/app/services/customer/customer.service.ts
+++ vtApp/src/app/services/customer/customer.service.ts
@@ -11,8 +11,24 @@ export class CustomerService {
 
   constructor(private httpClient: HttpClient) { }
 
+
+  getAllCustomers(): Observable<CustomerModel[]> {
+    return this.httpClient.
+      get<CustomerModel[]>(environment.apiUrl + 'Customer');
+  }
+  
   getCustomerById(id: number): Observable<CustomerModel> {
     return this.httpClient.
       get<CustomerModel>(environment.apiUrl + 'Customer' + "/" + id);
+  }
+
+  saveCustomer(vm: CustomerModel): Observable<CustomerModel> {
+    return this.httpClient.
+      post<CustomerModel>(environment.apiUrl + 'Customer', vm);
+  }
+
+  delete(id: number): Observable<CustomerModel> {
+    return this.httpClient.
+      delete<CustomerModel>(environment.apiUrl + 'Customer' + "/" + id);
   }
 }
